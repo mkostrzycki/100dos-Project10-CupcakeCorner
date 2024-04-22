3d49313af8900bac7b7f12db77c67edfe81d1c98
Maciej Kostrzycki <maciej.kostrzycki@gmail.com>
2024-04-22 20:37:37 +0200

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by MaćKo on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
